53b2d362121821a0855f4f2f4b7a084e2704154f
Rahul Teradal <149926500+Rahul3056@users.noreply.github.com>
2025-07-27 15:17:41 +0530

    Update sram_gen.sv

--- sram_gen.sv
+++ sram_gen.sv
@@ -1,1 +1,31 @@
+`ifndef __SRAM_GEN_SV__
+`define __SRAM_GEN_SV__
 
+`include "sram_txn.sv"
+
+class sram_gen;
+
+  mailbox #(sram_txn) gen2drv;
+  int num_txns;
+
+  // Constructor
+  function new(mailbox #(sram_txn) gen2drv, int num_txns);
+    this.gen2drv = gen2drv;
+    this.num_txns = num_txns;
+  endfunction
+
+  // Task to generate transactions
+  task run(); // <-- changed from generate() to run()
+    sram_txn txn;
+    repeat (num_txns) begin
+      txn = new();
+      if (!txn.randomize()) begin
+        $display("Error: Randomization failed in generator");
+      end
+      gen2drv.put(txn);
+    end
+  endtask
+
+endclass
+
+`endif
